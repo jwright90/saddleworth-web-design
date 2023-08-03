46c3c19d4bbcfda7ea27667d0c6d2b1dab9b60bf
Jason Wright <jason.wright90@gmail.com>
2023-08-03 19:44:13 +0100

    landing page image and headlines

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,27 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
+  return (
+    <main className="min-h-screen bg-white">
+      <div className="grid place-items-center h-screen w-screen relative
+                      bg-no-repeat bg-cover bg-center bg-[url('/saddleworth_1.jpg')] 
+                      bg-clip-text text-transparent"
+      >
+        <h1 className="text-[120px] font-bold text-center leading-[110px] mb-[50px] place-self-end justify-self-center">Saddleworth <br></br>Web Design</h1>
+        <Image
+          src="/saddleworth_1.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="Saddleworth Landscape Photoshot with Mcr in Background"
+          className="opacity-20"
+        />
+
+        <h3 className="text-gray-600 text-[30px] w-[760px] leading-10 font-light text-center place-self-start justify-self-center">
+          Helping small businesses to boost their sales through better websites
+        </h3>
+      
+      </div>
     </main>
   )
 }
