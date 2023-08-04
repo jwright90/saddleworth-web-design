31c1586311d35cc678b937bd0680f503ead400bf
Jason Wright <jason.wright90@gmail.com>
2023-08-04 17:57:01 +0100

    overlay success!

--- app/page.js
+++ app/page.js
@@ -3,29 +3,17 @@ import Image from 'next/image'
 export default function Home() {
 
   return (
-    <main className="min-h-screen ">
-
-        <div className="h-screen w-screen flex flex-column flex-wrap justify-center bg-no-repeat bg-cover bg-center bg-[url('/saddleworth_1.jpg')] 
-                        bg-clip-text text-transparent "
-        >
-          <Image
+    <main className="min-h-screen flex flex-col justifty-center items-center">
+        <div className="relative w-screen h-screen max-w-full rounded-lg shadow-2xl overflow-hidden">
+        <Image 
             src="/saddleworth_1.jpg"
+            alt=""
             layout="fill"
             objectFit="cover"
-            alt="Saddleworth Landscape Photoshot with Mcr in Background"
-            className="opacity-[25%] z-10 pointer-events-none"
+            className="absolute inset-0 w-full h-full rounded-lg shadow-xl"
           />
-
-          <div className="flex flex-column flex-wrap justify-center content-end pb-12">
-            <h1 className=" text-[120px] font-bold text-center leading-[110px] mb-[50px] basis-full place-self-end ">Saddleworth <br></br>Web Design</h1>
-
-            <h3 className="text-white text-[2rem] max-w-[760px] leading-10 font-light text-center self-start ">
-              Helping small businesses to boost their sales through better websites
-            </h3>
-          </div>
-
-          <div className="basis-full z-20 flex justify-center grow">
-            <button className="text-[1.4rem] font-semibold max-w-[450px] rounded-sm bg-diggle-green text-white py-5 self-start basis-full">Book Consultation</button>
+          <div className="absolute inset-0 w-full h-full rounded-lg mix-blend-overlay flex flex-col justify-center">
+            <h1 className="text-8xl font-extrabold text-center text-white">Saddleworth <br></br> Web Design</h1>
           </div>
         </div>
     </main>
