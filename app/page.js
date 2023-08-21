53d596cd4b55166adc78d7edb8cb53ae250d5b37
Jason Wright <jason.wright90@gmail.com>
2023-08-21 20:32:17 +0100

    update background to gray and remove mix-blend

--- app/page.js
+++ app/page.js
@@ -3,17 +3,19 @@ import Image from 'next/image'
 export default function Home() {
 
   return (
-    <main className="min-h-screen flex flex-col justifty-center items-center">
-        <div className="relative w-screen h-screen max-w-full rounded-lg shadow-2xl overflow-hidden">
+    <main className="min-h-screen">
+        <div className="relative w-screen h-screen max-w-full">
         <Image 
             src="/saddleworth_1.jpg"
             alt=""
             layout="fill"
             objectFit="cover"
-            className="absolute inset-0 w-full h-full rounded-lg shadow-xl"
+            className="absolute"
           />
-          <div className="absolute inset-0 w-full h-full rounded-lg mix-blend-overlay flex flex-col justify-center">
-            <h1 className="text-8xl font-extrabold text-center text-white">Saddleworth <br></br> Web Design</h1>
+          <div className="absolute w-full h-full bg-gray-900 bg-opacity-[30%] flex flex-col justify-center px-8">
+            <h1 className="text-8xl font-extrabold text-center text-white mb-12">Saddleworth <br></br> Web Design</h1>
+            <h3 className="text-4xl text-center text-white max-w-[800px] mx-auto mb-12">Helping small businesses to grow sales through online presence</h3>
+            <button className="rounded-full border-2 w-[400px] h-[60px] mx-auto text-2xl">Book a consultation</button>
           </div>
         </div>
     </main>
