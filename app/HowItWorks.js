46f43ea1f6a4e210a0658aa979aa7d9653beac53
Jason Wright <jason.wright90@gmail.com>
2023-09-16 19:55:02 +0100

    add responsiveness and contact section

--- app/HowItWorks.js
+++ app/HowItWorks.js
@@ -4,7 +4,7 @@ export default function HowItWorks ({image, number, heading, p1, p2}) {
 
     return (
         <>
-            <div className="relative h-screen">
+            <div className="relative h-screen md:block hidden">
                 <Image 
                     src={`${image}`}
                     alt=""
@@ -13,8 +13,8 @@ export default function HowItWorks ({image, number, heading, p1, p2}) {
                     className="absolute"
                 />
                 <div className={
-                    number%2==0 ? `flex-row-reverse absolute h-screen bg-gray-900 bg-opacity-[50%] flex justify-between 2xl:px-60 xl:px-40 lg:px-30 px-20 items-center` 
-                                : `absolute h-screen bg-gray-900 bg-opacity-[50%] flex justify-between 2xl:px-60 xl:px-40 lg:px-30 px-20 items-center`}
+                    number%2==0 ? `flex-row-reverse absolute h-screen bg-gray-900 bg-opacity-[60%] flex justify-between 2xl:px-60 xl:px-40 lg:px-30 px-20 items-center` 
+                                : `absolute h-screen bg-gray-900 bg-opacity-[70%] flex justify-between 2xl:px-60 xl:px-40 lg:px-30 px-20 items-center`}
                 >
                     <div className={number%2==0 ? "h-[100%] flex flex-col w-[100%] items-end" : "h-[100%] flex flex-col w-[100%]"}>
                         <div className="lg:text-[600px] text-[400px] lg:mt-0 mt-[150px] font-bold text-diggle-green h-[600px]">{`${number}`}</div>
@@ -22,6 +22,28 @@ export default function HowItWorks ({image, number, heading, p1, p2}) {
                     </div>
 
                     <div className="font-light xl:text-3xl lg:text-2xl max-w-[48%] h-[100%] flex flex-col justify-center ">
+                        {p1 && <p className="mb-8">{`${p1}`}</p>}
+                        {p2 && <p>{`${p2}`}</p>}
+                    </div>
+                </div>
+            </div>
+            <div className="relative h-screen md:hidden block">
+                <div className="relative h-[50%] ">
+                    <Image 
+                        src={`${image}`}
+                        alt=""
+                        layout="fill"
+                        objectFit="cover"
+                        className="absolute"
+                    />
+                    <div className="absolute h-[100%] w-[100%] bg-gray-900 opacity-[30%]"></div>
+                    <div className={"flex flex-col px-20"}>
+                        <div className="absolute text-[300px] lg:mt-0 font-bold text-diggle-green h-[600px]">{`${number}`}</div>
+                        <div className="2xl:text-7xl xl:text-6xl lg:text-5xl text-4xl font-extrabold absolute top-[50%] 2xl:w-[500px] lg:w-[350px] w-[300px]">{`${heading}`}</div>
+                    </div>
+                </div>
+                <div className="h-[50%] bg-white text-black flex justify-center items-center">
+                    <div className="px-20 flex flex-col justify-center text-xl">
                         {p1 && <p className="mb-8">{`${p1}`}</p>}
                         {p2 && <p>{`${p2}`}</p>}
                     </div>
--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 import Image from 'next/image'
 import HowItWorks from './HowItWorks'
 
+import {FaPencilAlt} from 'react-icons/fa'
+import {FaRocket} from 'react-icons/fa'
+import {FaPaintBrush} from 'react-icons/fa'
+import {BsFillSendFill} from 'react-icons/bs'
+
 export default function Home() {
 
   return (
@@ -17,9 +22,9 @@ export default function Home() {
               className="absolute"
             />
             <div className="absolute w-full h-full bg-gray-900 bg-opacity-[35%] flex flex-col justify-center px-8">
-              <h1 className="text-8xl font-extrabold text-center text-white mb-8">Saddleworth <br></br> Web Design</h1>
-              <h3 className="text-4xl text-center text-white max-w-[800px] mx-auto mb-12 font-light">Helping small businesses to grow sales through online presence</h3>
-              <button className="font-medium rounded-full border-2 w-[400px] h-[60px] mx-auto text-2xl bg-diggle-green shadow-black shadow-2xl">Get a free website</button>
+              <h1 className="text-6xl md:text-8xl font-extrabold text-center text-white mb-8">Saddleworth <br></br> Web Design</h1>
+              <h3 className="text-xl md:text-4xl text-center text-white max-w-[800px] mx-auto mb-12 font-light">Helping small businesses to grow sales through online presence</h3>
+              <button className="font-medium rounded-full border-2 w-[400px] h-[60px] mx-auto text-xl md:text-2xl bg-diggle-green shadow-black shadow-2xl">Get a free website</button>
               <div className="text-center mt-4 max-w-[600px] mx-auto text-sm font-light italic">
                 If you like the result then I ask for a testimonial, <br></br> pay what you feel it's worth (as much or as little as you like)
               </div>
@@ -29,21 +34,21 @@ export default function Home() {
 
       {/* YouTube Video & About Section */}
 
-      <div className="min-h-screen w-screen bg-cobalt-blue flex flex-col lg:px-40 md:px-30 px-20 py-12 text-white">
+      <div className="min-h-screen bg-cobalt-blue flex flex-col xl:px-40 lg:px-35 md:px-30 px-20 py-12 text-white">
         <div className="mb-[80px] bg-gray-300 w-[100%] h-[500px] rounded-lg text-gray-400 text-8xl text-center flex flex-col justify-center self-center">Youtube Video</div>
         <div className="flex justify-between xl:space-x-40 lg:space-x-20 md:space-x-12">
-          <div className="flex flex-col gap-8 md:gap-4 md:flex-row">
+          <div className="flex flex-col gap-8 md:gap-4 lg:gap-8 md:flex-row">
             <div className="flex flex-col">
-              <div className="font-bold text-2xl mb-2">Copywriting</div>
-              <div className="font-light text-sm lg:text-base">Our websites use advanced copywriting techniques to get customers to take action. We’ll help you to generate leads and convince people that your solution is right for them.</div>
+              <div className="font-bold text-lg lg:text-xl xl:text-2xl mb-3 flex gap-3 items-center"><FaPencilAlt /> Copywriting</div>
+              <div className="font-light text-sm lg:text-base xl:text-lg">Our websites use advanced copywriting techniques to get customers to take action. We’ll help you to generate leads and convince people that your solution is right for them.</div>
             </div>
             <div className="flex flex-col">
-              <div className="font-bold text-2xl mb-2">Latest Tech</div>
-              <div className="font-light text-sm lg:text-base">We use the latest technology to build our websites, which enhances speed, and makes search engines like Google rank you higher. It also reduces bugs and requires less maintenance.</div>
+              <div className="font-bold text-lg lg:text-xl xl:text-2xl mb-3 flex gap-3 items-center"><FaRocket/> Latest Tech</div>
+              <div className="font-light text-sm lg:text-base xl:text-lg">We use the latest technology to build our websites, which enhances speed, and makes search engines like Google rank you higher. It also reduces bugs and requires less maintenance.</div>
             </div>
             <div className="flex flex-col">
-              <div className="font-bold text-2xl mb-2">Bespoke Design</div>
-              <div className="font-light text-sm lg:text-base">Designs are produced from scratch. We do not use “off the shelf” templates to build our sites. You’ll get a unique website tailored explicitly to your needs.</div>
+              <div className="font-bold text-lg lg:text-xl xl:text-2xl mb-3 flex gap-3 items-center"><FaPaintBrush /> Bespoke Design</div>
+              <div className="font-light text-sm lg:text-base xl:text-lg">Designs are produced from scratch. We do not use “off the shelf” templates to build our sites. You’ll get a unique website tailored explicitly to your needs.</div>
             </div>
           </div>
         </div>
@@ -79,6 +84,24 @@ export default function Home() {
 
       </div>
 
+      {/* Contact section */}
+
+      <div className="min-h-screen bg-cobalt-blue flex flex-col xl:px-40 lg:px-35 md:px-30 px-20 py-12">
+        <h2 className="text-[42px] sm:text-6xl md:text-7xl font-bold text-center mt-8 text-white">Get in touch 👋</h2>
+        <div className="ml-auto mr-auto w-[350px] sm:w-[500px] flex justify-center text-sm sm:text-base">
+          <form className="mt-8 sm:mt-16 w-[100%]">
+            <label htmlFor="fullname">Full name:</label><br></br>
+            <input className="mt-1 mb-4 h-[35px] w-[100%] text-black p-2 rounded-sm" type="text" id="fullname" name="fullname"></input><br></br>
+            <label htmlFor="email">Email:</label><br></br>
+            <input className="mt-1 mb-4 h-[35px] w-[100%] text-black p-2 rounded-sm" type="email" id="email" name="email"></input><br></br>
+            <label htmlFor="phone">Phone:</label><br></br>
+            <input className="mt-1 mb-8 h-[35px] w-[100%] text-black p-2 rounded-sm" type="tel" id="phone" name="phone"></input><br></br>
+            <label htmlFor="message">Message:</label><br></br>
+            <textarea name="message" className="mt-1 mb-4 w-[100%] min-h-[180px] text-black rounded-sm p-2"></textarea>
+            <button className="block flex items-center justify-center gap-2 bg-diggle-green h-[60px] w-[100%] font-semibold text-base sm:text-xl rounded-sm"><BsFillSendFill /> Send Message</button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
