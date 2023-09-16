8c95dcbec7a9f3f303fd8ad2ccd927e001b78b86
Jason Wright <jason.wright90@gmail.com>
2023-09-16 16:45:04 +0100

    add responsiveness to how it works

--- app/HowItWorks.js
+++ app/HowItWorks.js
@@ -12,12 +12,16 @@ export default function HowItWorks ({image, number, heading, p1, p2}) {
                     objectFit="cover"
                     className="absolute"
                 />
-                <div className="absolute h-screen bg-gray-900 bg-opacity-[50%] flex justify-between 2xl:px-60 xl:px-40 lg:px-30 px-20 items-center">
-                    <div className="h-[100%] flex flex-col">
-                    <div className="text-[600px] font-bold text-diggle-green h-[600px]">{`${number}`}</div>
-                    <div className="2xl:text-7xl xl:text-6xl lg:text-5xl text-4xl font-extrabold absolute top-[50%] 2xl:w-[500px] lg:w-[350px] w-[300px]">{`${heading}`}</div>
+                <div className={
+                    number%2==0 ? `flex-row-reverse absolute h-screen bg-gray-900 bg-opacity-[50%] flex justify-between 2xl:px-60 xl:px-40 lg:px-30 px-20 items-center` 
+                                : `absolute h-screen bg-gray-900 bg-opacity-[50%] flex justify-between 2xl:px-60 xl:px-40 lg:px-30 px-20 items-center`}
+                >
+                    <div className={number%2==0 ? "h-[100%] flex flex-col w-[100%] items-end" : "h-[100%] flex flex-col w-[100%]"}>
+                        <div className="lg:text-[600px] text-[400px] lg:mt-0 mt-[150px] font-bold text-diggle-green h-[600px]">{`${number}`}</div>
+                        <div className="2xl:text-7xl xl:text-6xl lg:text-5xl text-4xl font-extrabold absolute top-[50%] 2xl:w-[500px] lg:w-[350px] w-[300px]">{`${heading}`}</div>
                     </div>
-                    <div className="font-light xl:text-3xl lg:text-2xl max-w-[48%]">
+
+                    <div className="font-light xl:text-3xl lg:text-2xl max-w-[48%] h-[100%] flex flex-col justify-center ">
                         {p1 && <p className="mb-8">{`${p1}`}</p>}
                         {p2 && <p>{`${p2}`}</p>}
                     </div>
--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       {/* Landing Page */}
 
       <div className="min-h-screen text-white">
-          <div className="relative w-screen h-screen max-w-full">
+          <div className="relative h-screen">
           <Image 
               src="/saddleworth_1.jpg"
               alt=""
@@ -29,20 +29,22 @@ export default function Home() {
 
       {/* YouTube Video & About Section */}
 
-      <div className="min-h-screen w-screen bg-cobalt-blue flex flex-col lg:px-40 md:px-20 py-12 text-white">
+      <div className="min-h-screen w-screen bg-cobalt-blue flex flex-col lg:px-40 md:px-30 px-20 py-12 text-white">
         <div className="mb-[80px] bg-gray-300 w-[100%] h-[500px] rounded-lg text-gray-400 text-8xl text-center flex flex-col justify-center self-center">Youtube Video</div>
         <div className="flex justify-between xl:space-x-40 lg:space-x-20 md:space-x-12">
-          <div className="flex flex-col">
-            <div className="font-bold text-2xl mb-2">Copywriting</div>
-            <div className="font-light">Our websites use advanced copywriting techniques to get customers to take action. We’ll help you to generate leads and convince people that your solution is right for them.</div>
-          </div>
-          <div className="flex flex-col">
-            <div className="font-bold text-2xl mb-2">Latest Tech</div>
-            <div className="font-light">We use the latest technology to build our websites, which enhances speed, and makes search engines like Google rank you higher. It also reduces bugs and requires less maintenance.</div>
-          </div>
-          <div className="flex flex-col">
-            <div className="font-bold text-2xl mb-2">Bespoke Design</div>
-            <div className="font-light">Designs are produced from scratch. We do not use “off the shelf” templates to build our sites. You’ll get a unique website tailored explicitly to your needs.</div>
+          <div className="flex flex-col gap-8 md:gap-4 md:flex-row">
+            <div className="flex flex-col">
+              <div className="font-bold text-2xl mb-2">Copywriting</div>
+              <div className="font-light text-sm lg:text-base">Our websites use advanced copywriting techniques to get customers to take action. We’ll help you to generate leads and convince people that your solution is right for them.</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="font-bold text-2xl mb-2">Latest Tech</div>
+              <div className="font-light text-sm lg:text-base">We use the latest technology to build our websites, which enhances speed, and makes search engines like Google rank you higher. It also reduces bugs and requires less maintenance.</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="font-bold text-2xl mb-2">Bespoke Design</div>
+              <div className="font-light text-sm lg:text-base">Designs are produced from scratch. We do not use “off the shelf” templates to build our sites. You’ll get a unique website tailored explicitly to your needs.</div>
+            </div>
           </div>
         </div>
       </div>
@@ -59,27 +61,18 @@ export default function Home() {
         p2={`It may be that you need to better educate your customer on your offering, improve the overall feel and authenticity of the website, to build trust, or generate more sales through better structure and call-to-actions.`}
         />
 
-        <div className="relative h-screen">
-          <Image 
-              src="/design.png"
-              alt=""
-              layout="fill"
-              objectFit="cover"
-              className="absolute blur-[3px] mt-[3px]"
-          />
-          <div className="absolute h-screen bg-gray-900 bg-opacity-[70%] flex flex-row-reverse px-40 items-center">
-            <div className="h-[100%] flex flex-col mr-auto">
-              <div className="text-[600px] font-bold text-diggle-green h-[600px]">2</div>
-              <div className="text-8xl font-extrabold absolute top-[50%]">Wireframe & Design</div>
-            </div>
-            <div className="font-light text-4xl max-w-[48%] mr-auto ">We will discuss what it is that you need your website to do and the brand image that you want to portray. <br></br> <br></br>It may be that you need to better educate your customer on your offering, improve the overall feel and authenticity of the website, to build trust, or generate more sales through better structure and call-to-actions.</div>
-          </div>
-        </div>
+        <HowItWorks 
+        image={"/design.png"}
+        number={"2"}
+        heading={"Wireframe & Design"}
+        p1={`We will discuss what it is that you need your website to do and the brand image that you want to portray. `}
+        p2={`It may be that you need to better educate your customer on your offering, improve the overall feel and authenticity of the website, to build trust, or generate more sales through better structure and call-to-actions.`}
+        />
 
         <HowItWorks 
         image={"/build.png"}
         number={"3"}
-        heading={"Consultation & Initial Brief"}
+        heading={"Build & Maintain"}
         p1={`We will discuss what it is that you need your website to do and the brand image that you want to portray.  `}
         p2={`It may be that you need to better educate your customer on your offering, improve the overall feel and authenticity of the website, to build trust, or generate more sales through better structure and call-to-actions.`}
         />
